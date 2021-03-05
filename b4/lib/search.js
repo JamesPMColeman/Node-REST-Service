e5022c50f8e37e343e1665fcb32a134369ae5329
James Coleman <jcolem22@msudenver.edu>
2021-03-05 12:21:46 -0700

    Added more GET functionality
    A suggest function to recommend alternative search inputs.

--- b4/lib/search.js
+++ b4/lib/search.js
@@ -9,11 +9,11 @@ module.exports = (app, es) => {
 
 	const url = `http://${es.host}:${es.port}/${es.books_index}/book/_search`;
 	/*
- 	*
- 	* Search for books by matching a particular field value.
- 	* Example: /api/search/books/authors/Twain
- 	*
- 	*/
+  	 *
+ 	 * Search for books by matching a particular field value.
+ 	 * Example: /api/search/books/authors/Twain
+ 	 *
+ 	 */
 	app.get('/api/search/books/:field/:query', (req, res) => {
 
 		const esReqBody = {
@@ -43,5 +43,44 @@ module.exports = (app, es) => {
 			res.status(200).json(esResBody.hits.hits.map(({_source}) => _source));
                                 		// destructuring assignment ^^^^
 		});
+	});
+	/*
+     *
+	 * Collect suggested terms for a given field based on a given query
+	 * Example: /api/suggest/authors/lipman
+	 *
+	 */
+	app.get('/api/suggest/:field/:query', (req, res) => {
+		
+		const esReqBody = {
+			size: 0,
+			suggest: {
+				suggestions: {
+					text: req.params.query,
+					term: {
+						field: req.params.field,
+						suggest_mode: 'always',
+					},
+				}
+			}
+		};
+		const options = {url, json: true, body: esReqBody};
+		const promise = new Promise((resolve, reject) => {
+			request.get(options, (err, esRes, esResBody) => {
+				
+				if (err) {
+					reject({error: err});
+					return;
+				}
+				if (esRes.statusCode !== 200) {
+					reject({error: esResBody});
+					return;
+				}
+				resolve(esResBody);
+			});
+		});
+		promise
+			.then(esResBody => res.status(200).json(esResBody.suggest.suggestions))
+			.catch(({error}) => res.status(error.status || 502).json(error));
 	});
 };
