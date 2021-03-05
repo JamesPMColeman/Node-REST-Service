9071e86eeeab6c56bd8adb233a3d190bfd470359
James Coleman <jcolem22@msudenver.edu>
2021-03-05 16:53:48 -0700

    PUT functionality
    The name path renames a book bundle with http put

--- b4/lib/bundle.js
+++ b4/lib/bundle.js
@@ -21,4 +21,40 @@ module.exports = (app, es) => {
 			.then(esResBody => res.status(201).json(esResBody))
 			.catch(({error}) => res.status(error.status || 502).json(error));
 	});
+		
+	/**
+	 * Retrieve a given bundle
+	 * curl http://<host>:<port>/api/bundle/<id>
+	 */
+	app.get('/api/bundle/:id', async (req, res) => {
+		const options = {
+			url: `${url}/${req.params.id}`,
+			json: true,
+		};
+		try {
+			const esResBody = await rp(options);
+			res.status(200).json(esResBody);
+		} catch (esResErr) {
+			res.status(esResErr.statusCode || 502).json(esResErr.error);
+		}
+	});
+	
+	/**
+	 * Set the specified bundle's name with specified name.
+	 * curl -X PUT http://<host>:<port>/api/bundle/<id>/name/<name>
+	 */
+	app.put('/api/bundle/:id/name/:name', async (req, res) => {
+		const bundleUrl = `${url}/${req.params.id}`;
+
+		try {
+			const bundle = (await rp({url: bundleUrl, json: true}))._source;
+			bundle.name = req.params.name;
+			const esResBody = 
+				await rp.put({url: bundleUrl, body: bundle, json: true});
+			res.status(200).json(esResBody);
+		} catch (esResErr) {
+			res.status(esResErr.statusCode || 502).json(esResErr.error);
+		}
+	});
+		
 };
